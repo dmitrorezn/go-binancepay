b85e77d703b288634a6f6baff2be273fbc09109d
dcb9 <bob@phpor.me>
2022-06-29 19:54:16 +0800

    Add binance official test data

--- binancepay_test.go
+++ binancepay_test.go
@@ -30,7 +30,8 @@ func TestUnmarshalRequest(t *testing.T) {
 }
 
 type fixedPublicKeyTestCache struct {
-	exist bool
+	exist     bool
+	publicKey string
 }
 
 func (c fixedPublicKeyTestCache) GetJSON(ctx context.Context, key string, i interface{}) (ok bool, err error) {
@@ -40,7 +41,7 @@ func (c fixedPublicKeyTestCache) GetJSON(ctx context.Context, key string, i inte
 
 	cert, _ := i.(*Certificate)
 	cert.CertSerial = "abc"
-	cert.CertPublic = testDataPublicKey
+	cert.CertPublic = c.publicKey
 	return true, nil
 }
 
@@ -49,7 +50,7 @@ func (c fixedPublicKeyTestCache) SetJSON(ctx context.Context, key string, data i
 }
 
 func TestWebhookWithCachedPublicKey(t *testing.T) {
-	client := NewMerchant("", "", fixedPublicKeyTestCache{exist: true}, logger)
+	client := NewMerchant("", "", fixedPublicKeyTestCache{exist: true, publicKey: testDataPublicKey}, logger)
 	body := `{"bizType":"PAY","bizId":"111","data":"testdata","bizStatus":"SUCCESS"}`
 	httpReq, err := http.NewRequest("POST", "/", strings.NewReader(body))
 	assert.Nil(t, err, err)
@@ -104,4 +105,38 @@ func TestWebhookWithNoCachedPublicKey(t *testing.T) {
 	rawReq, err := client.VerifyAndParseWebhookRequest(httpReq)
 	assert.Nil(t, err, err)
 	assert.Equal(t, "testdata", rawReq.RawData)
+}
+
+func TestWebhook(t *testing.T) {
+	pk := `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCVAMXVTMIuttHcP1kvSk9V39S7
+BqDbY+LC9JfwMzyoubo3fdsqBf4EmB1PfBUigcOL5YBqMKGEE6xmVw6SXCTH4JwX
+dqJ4IRiXf76YMt6PB0dMzu/qCmhaGFGkqT6vJM0hHyEbtS/P3FR9ZU+eaOvynLWb
+FvXTzIoctZM1IvDrCwIDAQAB
+-----END PUBLIC KEY-----`
+
+	client := NewMerchant("", "", fixedPublicKeyTestCache{exist: true, publicKey: pk}, logger)
+	body := `{"bizType":"PAY","data":"{\"merchantTradeNo\":\"9825382937292\",\"totalFee\":0.88000000,\"transactTime\":1619508939664,\"currency\":\"EUR\",\"commission\":0,\"openUserId\":\"1211HS10K81f4273ac031\",\"productType\":\"Food\",\"productName\":\"Ice Cream\",\"tradeType\":\"WEB\",\"transactionId\":\"M_R_282737362839373\"}","bizId":29383937493038367292,"bizStatus":"PAY_SUCCESS"}`
+	httpReq, err := http.NewRequest("POST", "/", strings.NewReader(body))
+	assert.Nil(t, err, err)
+	timestamp := "1611232922428"
+	nonce := "5RhaTrZPhknNv0kDSA2UQ67cPMVNS4sA"
+
+	h := http.Header{}
+	h.Set("BinancePay-Certificate-SN", "")
+	h.Set("BinancePay-Nonce", nonce)
+	h.Set("BinancePay-Timestamp", timestamp)
+	h.Set("BinancePay-Signature", "VtniwOFAi4oBKFnvHhY6UZ+wPARf7+yWVbE61Hc7JVdRgqKJ40Yk9k5Gb2Y0S0VLUYOOOhUNUsD7R8GDEr6WH84sAEf8bXa1xYMiyjgeGleTg1MxRJCtrHcxMeMCEXPWmKTeLhn6O+cdvDGq4ZpcTTiQnEvk5xHPcxFrBT637zg=\n")
+	h.Set("Content-Type", "application/json; charset=utf-8")
+	httpReq.Header = h
+
+	rawReq, err := client.VerifyAndParseWebhookRequest(httpReq)
+	assert.Nil(t, err, err)
+	assert.Equal(t, "PAY_SUCCESS", rawReq.BizStatus)
+
+	var orderNoti OrderNoti
+	err = json.Unmarshal([]byte(rawReq.RawData), &orderNoti)
+	assert.Nil(t, err, err)
+	assert.Equal(t, "9825382937292", orderNoti.MerchantTradeNo)
+	assert.Equal(t, 0.88, orderNoti.TotalFee.InexactFloat64())
 }
